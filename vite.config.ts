b4960f3ab098a542939ca8604d19e65033cf6ecd
maxim <max@sct.team>
2024-07-24 12:15:31 +0400

    Add dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://samedy34.github.io/ClientBot/",
+  base: "/ClientBot/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
