c877f822c280b28e200c272c35a1a991ca9d7b2d
maxim <max@sct.team>
2024-07-24 12:35:51 +0400

    Update router

--- src/router.ts
+++ src/router.ts
@@ -4,8 +4,8 @@ const Page1 = () => import("@/views/Page1.vue");
 const Page2 = () => import("@/views/Page2.vue");
 
 const routes = [
-  { path: "/", name: "index", component: Page1 },
-  { path: "/page2", name: "page2", component: Page2 },
+  { path: "/ClientBot/", name: "index", component: Page1 },
+  { path: "/ClientBot/page2", name: "page2", component: Page2 },
 ];
 
 const router = createRouter({
